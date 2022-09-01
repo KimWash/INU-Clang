026e8b60d8d64a79631902bed89d2c7bc5249969
KimWash <ckm0728wash@gmail.com>
2022-09-01 13:40:24 +0900

    Initial Commit

---
+++ INU Clang/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  INU Clang
+//
+//  Created by 최경민 on 2022/09/01.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
